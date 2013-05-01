8bfe6d4dde066e83310aaa1e28da0059d9beddf6
Alexey Gordeyev <aleksej@gordejev.lv>
2013-05-01 13:05:34 +0300

    Fix error pages

--- app/bin/trinte.js
+++ app/bin/trinte.js
@@ -205,19 +205,30 @@ function configureApp(trinte) {
             res.status(err.status || 500);
 
             if(parseInt(err.status) === 403) {
-                res.render('errors/403');
+                res.render('errors/403', {
+                    request : req,
+                    session : req.session
+                });
             } else if (parseInt(err.status) === 401) {
-                res.render('errors/401');
+                res.render('errors/401', {
+                    request : req,
+                    session : req.session
+                });
             } else {
                 res.render('errors/500', {
-                    error: err
+                    error: err,
+                    request : req,
+                    session : req.session
                 });
             }
         });
 
         // Example 404 page via simple Connect middleware
         app.use(function(req, res) {
-            res.render('errors/404');
+            res.render('errors/404', {
+                request : req,
+                session : req.session
+            });
         });
 
         // Initialize users params
